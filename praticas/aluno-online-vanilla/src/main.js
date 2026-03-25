9f795ba2410964f518a7294bb61749393703a36b
Lucas <llcsz.rocha@gmail.com>
2026-03-24 21:03:40 -0300

    feat: conclui pratica02. Fecha #3

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,39 +1,34 @@
 import "./style.css";
 
-const formulario = document.querySelector("#formLogin");
+document
+  .getElementById("formularioLogin")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
 
-formulario.addEventListener("submit", function (e) {
-  e.preventDefault();
-  
-  const emailInput = document.querySelector("#email");
-  const emailErro = document.querySelector("#emailErro");
-  const senhaInput = document.querySelector("#senha");
-  const senhaErro = document.querySelector("#senhaErro");
+    const EmailInput = document.querySelector("#email");
+    const senhaInput = document.querySelector("#senha");
+    const erroEmail = document.querySelector("#emailErro");
+    const erroSenha = document.querySelector("#senhaErro");
 
-  emailErro.textContent = "";
-  senhaErro.textContent = "";
+    erroEmail.textContent = "";
+    erroSenha.textContent = "";
 
+    //O formulário não pode ser enviado se houver campos vazios
+    if (EmailInput.value === "" && senhaInput.value === "") {
+      erroEmail.textContent = "O Campo de email é obrigatório";
+      erroSenha.textContent = "O Campo de senha é obrigatório";
+      return;
+    }
 
-  if (emailInput.value == "" && senhaInput.value == "") {
-    emailErro.textContent = "E-mail é obrigatório.";
-    senhaErro.textContent = "Senha é obrigatória.";
-    return;
-  }
-  
-  if (emailInput.value == "") {
-    emailErro.textContent = "E-mail é obrigatório.";
-    return;
-  }
+    if (EmailInput.value === "") {
+      erroEmail.textContent = "O Campo de email é obrigatório";
+      return;
+    }
 
-  if (senhaInput.value == "") {
-    senhaErro.textContent = "Senha é obrigatória.";
-    return;
-  }
+    if (senhaInput.value === "") {
+      erroSenha.textContent = "O Campo de senha é obrigatório";
+      return;
+    }
 
-  if (senhaInput.value.length < 8) {
-    senhaErro.textContent = "A senha deve ter no mínimo 8 caracteres.";
-    return;
-  }
-
-  console.log("Formulário validado com sucesso!");
-});
+    window.location.href = "http://localhost:5173/";
+  });
