8e983eb66e7728bf04e5d2967674e20fbe0bf6b2
Lucas <llcsz.rocha@gmail.com>
2026-04-02 20:30:13 -0300

    Subindo os arquivos da aula03

---
+++ aulas/aula03/src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import logo from "../assets/learn.svg";
+import Menu from "./Menu";
+function Sidebar() {
+  return (
+    <aside>
+      <header>
+        <img src={logo} alt="Logotipo" />
+        <h1>Aluno Online</h1>
+      </header>
+      <Menu />
+    </aside>
+  );
+}
+
+export default Sidebar;
---
+++ aulas/aula03/src/pages/Dashboard.jsx
@@ -0,0 +1,22 @@
+import Topbar from "../components/Topbar";
+import Card from "../components/Card";
+import Sidebar from "../components/Sidebar";
+
+function Dashboard() {
+  return (
+    <div>
+      <Sidebar />
+      <main>
+        <Topbar />
+        <h2>Bem vindo ao Portal do aluno</h2>
+        <section>
+          <Card />
+          <Card />
+          <Card />
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default Dashboard;
